eb395aba06362de12949312d54b74ba2dcd5a03c
Leowilson17 <leobca2018@gmail.com>
2023-03-02 18:38:25 +0530

    Add Data LIbrary Functionality

--- src/webparts/hrPandalogusa/components/Dashboard.tsx
+++ src/webparts/hrPandalogusa/components/Dashboard.tsx
@@ -21,16 +21,13 @@ import {
   TooltipDelay,
   DirectionalHint,
   mergeStyleSets,
+  Modal,
+  NormalPeoplePicker,
+  TextField,
 } from "@fluentui/react";
 import { sp } from "@pnp/sp/presets/all";
 import { useState } from "react";
-import { TextField } from "office-ui-fabric-react";
-// import {
-//   IPersonaSharedProps,
-//   Persona,
-//   PersonaSize,
-//   PersonaPresence,
-// } from "office-ui-fabric-react/lib/Persona";
+import * as _ from "lodash";
 
 // master Interface
 interface IItems {
@@ -45,7 +42,16 @@ interface IItems {
   PendingMembersID: number[];
   approvedMembersID: number[];
   approversID: number[];
+  DocTitle: string;
 }
+
+// error handling
+const obj = {
+  titleValidation: false,
+  fileValidation: false,
+  signValidation: false,
+  overAllValidation: false,
+};
 
 function Dashboard(props: any) {
   let allPeoples = props.peopleList;
@@ -62,7 +68,7 @@ function Dashboard(props: any) {
   const columns: IColumn[] = [
     {
       key: "column1",
-      name: "File Name",
+      name: "File name",
       fieldName: "Title",
       minWidth: 200,
       maxWidth: 350,
@@ -182,7 +188,7 @@ function Dashboard(props: any) {
     },
     {
       key: "column4",
-      name: "Submitted On",
+      name: "Submitted on",
       fieldName: "created",
       minWidth: 150,
       maxWidth: 200,
@@ -191,7 +197,7 @@ function Dashboard(props: any) {
 
     {
       key: "column5",
-      name: "Pending",
+      name: "Not Acknowledged by",
       fieldName: "PendingMembers",
       minWidth: 150,
       maxWidth: 200,
@@ -270,7 +276,7 @@ function Dashboard(props: any) {
     },
     {
       key: "column6",
-      name: "Approved",
+      name: "Acknowledged by",
       fieldName: "ApprovedMembers",
       minWidth: 150,
       maxWidth: 200,
@@ -350,23 +356,64 @@ function Dashboard(props: any) {
     {
       key: "column7",
       name: "Action",
-      minWidth: 100,
-      maxWidth: 150,
+      minWidth: 70,
+      maxWidth: 100,
       //   onColumnClick: this._onColumnClick,
       onRender: (item) => (
         <div>
-          <IconButton iconProps={editIcon} style={{ color: "#ff7e00" }} />
+          <IconButton
+            id={item.ID}
+            iconProps={editIcon}
+            style={{ color: "#ff7e00" }}
+            onClick={() => {
+              console.log(item);
+
+              let getDataObj = {
+                type: "edit",
+                Id: item.ID,
+                Title: item.DocTitle,
+                Mail: item.Approvers,
+                File: {},
+                FileName: item.Title,
+              };
+              setValueObj(getDataObj);
+              setHideModal(true);
+            }}
+          />
           {/* <IconButton iconProps={deleteIcon} /> */}
         </div>
       ),
     },
   ];
+  //  peoplepicker variable
+  const GetUserDetails = (filterText: any) => {
+    var result = allPeoples.filter(
+      (value, index, self) => index === self.findIndex((t) => t.ID === value.ID)
+    );
+
+    return result.filter((item) =>
+      doesTextStartWith(item.text as string, filterText)
+    );
+  };
+  const doesTextStartWith = (text: string, filterText: string) => {
+    return text.toLowerCase().indexOf(filterText.toLowerCase()) === 0;
+  };
 
   let filterKeys = {
     Title: "",
     Status: "All",
     Approvers: "",
   };
+
+  let getDataObj = {
+    type: "new",
+    Id: 0,
+    Title: "",
+    Mail: [],
+    File: {},
+    FileName: "",
+  };
+
   // style variables
   // icon variables
   const editIcon = { iconName: "edit" };
@@ -398,17 +445,15 @@ function Dashboard(props: any) {
           // backgroundColor: "#ff7e0045",
           color: "#63666a!important",
           backgroundColor: "#f7f9fa!important",
-          "&:hover": {
-            color: "#ff7e00",
-            background: "#ff7e0045 !important",
-          },
+          // "&:hover": {
+          //   color: "#ff7e00",
+          //   background: "#ff7e0045 !important",
+          // },
         },
       },
       ".ms-DetailsRow": {
         boxShadow: "rgb(136 139 141 / 12%) 0px 3px 20px",
-        ":hover": {
-          boxShadow: "rgb(136 139 141 / 12%) 0px 3px 20px",
-        },
+        ":hover": {},
       },
     },
   };
@@ -444,11 +489,51 @@ function Dashboard(props: any) {
       },
     ],
   });
+  const newmodalDesign = {
+    main: {
+      width: 450,
+      height: 320,
+    },
+  };
+  const editmodalDesign = {
+    main: {
+      width: 450,
+      height: 360,
+    },
+  };
+  const textFieldDesign = {
+    root: {
+      width: "90%",
+      margin: "0 10px",
+    },
+    fieldGroup: {
+      // border: "1px solid rgb(96, 94, 92) !important",
+      "&::after": {
+        border: "2px solid rgb(96, 94, 92) !important",
+      },
+    },
+  };
+  const peoplePickerStyle = {
+    root: {
+      width: 307,
+      margin: "0 10px 0 0",
+      ".ms-BasePicker-text": {
+        maxHeight: "100px",
+        overflowX: "hidden",
+        padding: "3px 5px",
+        "::after": {
+          border: "none",
+        },
+      },
+    },
+  };
 
   // State variable
   const [masterData, setMasterData] = useState<IItems[]>([]);
   const [FilterKeys, setFilterKeys] = useState(filterKeys);
   const [displayData, setdisplayData] = useState([]);
+  const [valueObj, setValueObj] = useState(getDataObj);
+  const [showModal, setHideModal] = useState(false);
 
   // function
   // get Document from Library
@@ -461,6 +546,7 @@ function Dashboard(props: any) {
       .get()
       .then((value: any) => {
         value.forEach((data) => {
+          // console.log(data);
           // date formatter
           let getDate = data.TimeCreated.substring(0, 10);
           const [year, month, date] = getDate.split("-");
@@ -511,6 +597,7 @@ function Dashboard(props: any) {
             PendingMembersID: data.ListItemAllFields["PendingApproversId"],
             approvedMembersID: data.ListItemAllFields["ApprovedApproversId"],
             approversID: data.ListItemAllFields["ApproversId"],
+            DocTitle: data.ListItemAllFields["UserTitle"],
           });
         });
         setMasterData(getDataArray);
@@ -543,21 +630,52 @@ function Dashboard(props: any) {
     setdisplayData([...tempArr]);
     setFilterKeys({ ...tempFilter });
   }
+
+  // modal Onchangehandler
+  function Onchangehandler(key, val) {
+    let getDatatempArray = valueObj;
+    getDatatempArray[key] = val;
+    setValueObj({ ...getDatatempArray });
+  }
+
   // add file
-  // function addFile(doc) {
-  //   console.log(doc);
-  //   let file = doc.target.files[0];
-  //   let filePath = doc.target.files[0].name;
-  //   const fileNamePath = encodeURI(filePath);
-  //   console.log(filePath);
-  //   sp.web
-  //     .getFolderByServerRelativePath("/sites/HRDev/Shared Documents")
-  //     .files.addUsingPath(fileNamePath, file, { Overwrite: true })
-  //     .then((data) => {
-  //       alert("Success");
-  //       getDatafromLibrary();
-  //     })
-  //     .catch(function (error) {});
+  function addFile() {
+    let updateData = valueObj;
+    let fileName = updateData.File["name"];
+    let approvers = [];
+    updateData.Mail.forEach((people) => {
+      approvers.push(people.ID);
+    });
+    sp.web
+      .getFolderByServerRelativePath("/sites/HRDev/Shared Documents")
+      .files.add(fileName, updateData.File, true)
+      .then((data) => {
+        data.file.getItem().then((item) => {
+          item
+            .update({
+              UserTitle: updateData.Title,
+              ApproversId: {
+                results: approvers,
+              },
+              Status: "Pending",
+            })
+            .then((result) => {
+              console.log(result);
+              getDatafromLibrary();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // edit function
+  // function editFunction(id) {
+  // let editArr =
   // }
 
   // useEffect
@@ -619,7 +737,15 @@ function Dashboard(props: any) {
               addFile(file);
             }}
           /> */}
-            <PrimaryButton text="New" className={styles.newBtn} />
+            <PrimaryButton
+              text="New"
+              className={styles.newBtn}
+              onClick={() => {
+                setHideModal(true);
+                valueObj.Id = 0;
+                valueObj.type = "new";
+              }}
+            />
           </div>
           {/* filter section ends */}
 
@@ -632,6 +758,95 @@ function Dashboard(props: any) {
           />
         </div>
       </div>
+
+      {/* modal section */}
+      <Modal
+        styles={valueObj.type == "new" ? newmodalDesign : editmodalDesign}
+        isOpen={showModal}
+        // onDismiss={false}
+      >
+        <div className={styles.modalCustomDesign}>
+          <div className={styles.header}>
+            <h2>New</h2>
+            {/* <IconButton iconProps={{ iconName: "cancel" }}></IconButton> */}
+          </div>
+
+          {/* details section */}
+          {/* title */}
+          <div
+            style={valueObj.type == "new" ? { height: 230 } : { height: 270 }}
+          >
+            <div className={styles.detailsSection}>
+              <Label>Title:</Label>
+              <TextField
+                styles={textFieldDesign}
+                value={valueObj.Title}
+                readOnly={valueObj.type == "edit"}
+                onChange={(name) => {
+                  Onchangehandler("Title", name.target["value"]);
+                }}
+              ></TextField>
+            </div>
+            {/* file */}
+            <div className={styles.detailsSection}>
+              <Label>File:</Label>
+              <input
+                className={styles.fileStyle}
+                type="file"
+                id="uploadFile"
+                onChange={(file) => {
+                  Onchangehandler("File", file.target["files"][0]);
+                }}
+              />
+            </div>
+            {valueObj.Id != 0 && (
+              <>
+                <div className={styles.detailsSection}>
+                  <Label></Label>
+                  <Label style={{ width: 310 }}>{valueObj.FileName}</Label>
+                </div>
+              </>
+            )}
+
+            {/* people picker */}
+            <div
+              className={styles.detailsSection}
+              style={{ alignItems: "flex-start" }}
+            >
+              <Label>Signatories:</Label>
+              <NormalPeoplePicker
+                styles={peoplePickerStyle}
+                onResolveSuggestions={GetUserDetails}
+                itemLimit={10}
+                selectedItems={valueObj.Mail}
+                onChange={(selectedUser) => {
+                  Onchangehandler("Mail", selectedUser);
+                }}
+              />
+            </div>
+          </div>
+
+          {/* btn section */}
+          <div className={styles.btnSection}>
+            <PrimaryButton
+              className={styles.cancelBtn}
+              text="Cancel"
+              onClick={() => {
+                setHideModal(false);
+              }}
+            />
+            <PrimaryButton
+              className={styles.submitBtn}
+              text="Submit"
+              color="primary"
+              onClick={() => {
+                addFile();
+                setHideModal(false);
+              }}
+            />
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
